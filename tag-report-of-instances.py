df03f9cfe6e6799c6f66bc172c5a8489d9047115
jinoct <35163312+jinoct@users.noreply.github.com>
2018-05-09 07:47:02 +0530

    Update tag-report-of-instances.py

--- tag-report-of-instances.py
+++ tag-report-of-instances.py
@@ -7,8 +7,8 @@ import boto3
 
 access_key = input("Enter AWS_ACCESS_KEY_ID: ")
 secret_key = input("Enter AWS_SECRET_ACCESS_KEY: ")
-session_token = input('Enter AWS_SESSION_TOKEN: ')
-session = boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, aws_session_token=session_token)
+
+session = boto3.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
 ec2 = session.client('ec2', region_name='us-west-2')
 regions = [region['RegionName'] for region in ec2.describe_regions()['Regions']]
